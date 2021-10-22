c4239a7e15d2131f5f3b67b0c003a60c5a2c138d
Leonore Yardimli <leonore1982@gmail.com>
2021-10-22 12:00:25 +0800

    Initial Commit

---
+++ TypeSimpleAnswer/TypeSimpleAnswerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TypeSimpleAnswerApp.swift
+//  TypeSimpleAnswer
+//
+//  Created by Leonore Yardimli on 2021/10/22.
+//
+
+import SwiftUI
+
+@main
+struct TypeSimpleAnswerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
